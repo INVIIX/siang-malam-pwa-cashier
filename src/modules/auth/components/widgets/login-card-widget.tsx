39802ceffc297ef87e381d7a0054778df714d28c
Josua Sinaga <josua123690707@gmail.com>
2025-08-08 19:09:45 +0700

    feat: adding apk for user to download

--- src/modules/auth/components/widgets/login-card-widget.tsx
+++ src/modules/auth/components/widgets/login-card-widget.tsx
@@ -1,18 +1,16 @@
 "use client"
 
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
-import { cn } from "@/lib/utils";
-import { LoginForm } from "../ui/login-form";
-import { NavLink } from "react-router";
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
+import { cn } from "@/lib/utils"
+import { LoginForm } from "../ui/login-form"
+import { NavLink } from "react-router"
 
 export default function LoginCardWidget({ className, ...props }: React.ComponentProps<"div">) {
     return (
         <>
-            <Card className={cn("",)} {...props}>
+            <Card className={cn("")} {...props}>
                 <CardHeader className="flex flex-col items-center gap-2 text-center">
-                    <CardTitle className="text-2xl font-bold">
-                        Login to your account
-                    </CardTitle>
+                    <CardTitle className="text-2xl font-bold">Login to your account</CardTitle>
                     <CardDescription className="text-muted-foreground text-sm text-balance">
                         Enter your email below to login to your account
                     </CardDescription>
@@ -22,13 +20,18 @@ export default function LoginCardWidget({ className, ...props }: React.Component
                 </CardContent>
                 <CardFooter>
                     <div className="w-full text-center text-sm">
-                        Don't have an account?{" "}
-                        <NavLink to="/register" className="underline underline-offset-4">
-                            Sign Up
-                        </NavLink>
+                        <div>
+                            Don't have an account?{" "}
+                            <NavLink to="/register" className="underline underline-offset-4">
+                                Sign Up
+                            </NavLink>
+                        </div>
+                        <a href="/pos-service.apk" className="block mt-1 underline underline-offset-4" download>
+                            Download POS integration app
+                        </a>
                     </div>
                 </CardFooter>
             </Card>
         </>
-    );
+    )
 }
